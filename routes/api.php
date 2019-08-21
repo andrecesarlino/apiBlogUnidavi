aaf0b5433e045b3ad76a19616049904bd514b13f
Andre Lino <andreclino@gmail.com>
2019-08-20 21:03:26 -0300

    Works

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,9 @@ use Illuminate\Http\Request;
 
 Use App\Article;
 
-Route::get('articles', 'ArticleController@index');
-Route::get('articles/{id}', 'ArticleController@show');
-Route::post('articles', 'ArticleController@store');
-Route::put('articles/{id}', 'ArticleController@update');
-Route::delete('articles/{id}', 'ArticleController@delete');
 
+
+/*
 Route::middleware('auth:api')->get('/user', function (Request $request) {
         return $request->user();
 });
@@ -17,3 +14,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+*/
+Route::group(['middleware' => 'auth:api'], function() {
+    Route::get('articles', 'ArticleController@index');
+    Route::get('articles/{article}', 'ArticleController@show');
+    Route::post('articles', 'ArticleController@store');
+    Route::put('articles/{article}', 'ArticleController@update');
+    Route::delete('articles/{article}', 'ArticleController@delete');
+});
+
+Route::post('register', 'Auth\RegisterController@register');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout');
