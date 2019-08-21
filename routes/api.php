1e90003720ee3fa49248ccb9e2178657b7fb6070
Andre Lino <andreclino@gmail.com>
2019-08-20 21:04:51 -0300

    Worksx

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+})
 */
 Route::group(['middleware' => 'auth:api'], function() {
     Route::get('articles', 'ArticleController@index');
